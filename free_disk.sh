dec3f488ad55d118cc21f26c9f6f39eb21dd7b79
Jestin Gigi <jestingigi@gmail.com>
2023-08-28 17:20:12 +0530

    Added mail alert functionality

--- free_disk.sh
+++ free_disk.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+
+<<comment 
+Monitoring free disk and sending an alert message via mail
+
+Requires setup of postfix and mailutils on ubuntu
+comment
+
 set -x
-THRESHOLD=20
+THRESHOLD=50
 MAX=100
+TO='jestin3008@gmail.com'
 # user % of root (/)
 FREE=$(($MAX - $(df -h | awk '$6 ~ /^\/$/ {print $5}' | tr -d %)))
 
 # Check if used space exceedes threshold
 if [[ $FREE -lt $THRESHOLD ]];then
-    echo "Warning, disk space very low"
+    echo "Warning, disk space very low $FREE %" | mail -s "Disk Space Low" $TO
 else
     echo "Disk space OK"
 fi
